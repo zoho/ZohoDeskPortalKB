77f4eaa028c607ba2adc06b322d5bb0ea85a3c27
wasim-18972 <wasim-18972@zohocorp.com>
2025-05-07 14:21:20 +0530

    update version to 4.1.2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "4.0.7"
+let version = "4.1.1"
 let package = Package(
     name: "ZohoDeskPortalKB",
     platforms: [
@@ -16,12 +16,12 @@ let package = Package(
             targets: ["ZohoDeskPortalKB", "ZohoDeskPortalKBPackage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: "4.0.7")
+        .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: "4.1.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ZohoDeskPortalKB", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalKB/\(version)/ZohoDeskPortalKB.zip", checksum: "5e8965ffcca855494618522c165f53e07704dd302d1eac7079fc0c0b56438b53"),
+        .binaryTarget(name: "ZohoDeskPortalKB", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalKB/\(version)/ZohoDeskPortalKB.zip", checksum: "af2bcd469adebde600d7020f3fe1593da9b2e8e821e5351fa8f6397d8d3809aa"),
         
         .target(name: "ZohoDeskPortalKBPackage",
             dependencies: [
