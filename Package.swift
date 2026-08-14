472a1540e381e5a6775775c46f846d74d58877f0
wasim-18972 <wasim-18972@zohocorp.com>
2026-08-14 09:46:34 +0530

    fix package error

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "4.6.7"
+let version = Version("4.6.7")
 let package = Package(
     name: "ZohoDeskPortalKB",
     platforms: [
@@ -16,12 +16,12 @@ let package = Package(
             targets: ["ZohoDeskPortalKB", "ZohoDeskPortalKBPackage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: "4.6.7")
+        .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: version)
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ZohoDeskPortalKB", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalKB/\(version)/ZohoDeskPortalKB.zip", checksum: "838634374ab40e69a395132e5870ce06369f37bffd17fad27e4688fc4e9344e6"),
+        .binaryTarget(name: "ZohoDeskPortalKB", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalKB/\(version.description)/ZohoDeskPortalKB.zip", checksum: "838634374ab40e69a395132e5870ce06369f37bffd17fad27e4688fc4e9344e6"),
 
         .target(name: "ZohoDeskPortalKBPackage",
             dependencies: [
